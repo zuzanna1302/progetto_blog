c87a742f96cd76c688716d41c870b2cf3bbf913e
zuzanna1302 <74589556+zuzanna1302@users.noreply.github.com>
2020-11-24 17:42:05 +0100

    Aggiunta codice JQuery per chiamare php. Aggiunta di id al button e textarea per la pwd.
    Signed-off-by: zuzanna1302 <74589556+zuzanna1302@users.noreply.github.com>

--- blog_v2/login.php
+++ blog_v2/login.php
@@ -1,18 +1,19 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
     <link rel="stylesheet" href="css.css">
     	<!--Bootsrap 4 CDN-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
+
     <!--Fontawesome CDN-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
 	<!--Custom styles-->
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="css.css">
 
 </head>
 <body>
@@ -35,19 +36,19 @@
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
 						<input type="text" class="form-control" placeholder="username">
-						
+
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input type="password" class="form-control" placeholder="password">
+						<input type="password" id="text" class="form-control" placeholder="password">
 					</div>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
 					</div>
 					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-center login_btn">
+						<input type="submit" class="button" name="insert" value="insert">
 					</div>
 				</form>
 			</div>
@@ -60,3 +61,19 @@
 	</div>
 </div>body>
     </html>
+
+<script>
+$(document).ready(function(){
+        $('.button').click(function(){
+            var clickBtnValue = $(this).val();
+            var pwd = $("#text").val();
+            alert(pwd);
+            var ajaxurl = 'ajax.php',
+            data =  {'action': clickBtnValue,  'passwordUser': pwd};
+            $.post(ajaxurl, data, function (response) {
+                // Response div goes here.
+                alert(response);
+            });
+        });
+    });
+</script>
